16ba821e325969784ecbcff8d4109b8ca73b472e
Yana Tykhonenko <tihonenkoyana@gmail.com>
2023-03-11 20:29:45 +0100

    Refactor Main ()

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -6,17 +7,21 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        //Task1
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         ArrayList<Pet> petsArray = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             String line = br.readLine();
+            petsArray.add(Pet.createPetFromLine(line, ","));
+        }
+        Pet.toPrintPet(petsArray);
 
-            petsArray.add(Pet.createPetFromLine(line,","));
-        }
-        for (Pet record:petsArray) {
-           System.out.println(record.toStringPet());
-        }
+        //Task2
+        File file = new File("resourse/students");
+        ArrayList<Student> students = new ArrayList<>();
+        students = Student.createStudentFromFile(file);
+        Student.toPrintStudent(students);
 
     }
 }
