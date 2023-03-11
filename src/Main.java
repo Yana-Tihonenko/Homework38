25af9becc7c8dca53511e4ab131044155adc0dae
Yana Tykhonenko <tihonenkoyana@gmail.com>
2023-03-11 17:55:25 +0100

    Task1. Add  input  and output  data

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        ArrayList<Pet> petsArray = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            String line = br.readLine();
+
+            petsArray.add(Pet.createPetFromLine(line,","));
+        }
+        for (Pet record:petsArray) {
+           System.out.println(record.toStringPet());
+        }
+
     }
 }
